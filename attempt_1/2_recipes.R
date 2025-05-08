1de3dc2a83c462732cf101945675f6cf4cc6ff6e
zhang-j-j <jimmyzhang2027@u.northwestern.edu>
2025-05-07 21:02:18 -0500

    Define preprocessing recipes

--- attempt_1/2_recipes.R
+++ attempt_1/2_recipes.R
@@ -1,4 +1,4 @@
-# Regression Prediction Problem ----
+# Classification Prediction Problem ----
 # Stat 301-3
 # Attempt 1
 # Step 2: setup preprocessing recipes
@@ -14,18 +14,37 @@ tidymodels_prefer()
 # Load data ----
 load(here("attempt_1/data_splits/airbnb_train.rda"))
 
-# Linear recipe ----
-
-# recipe for linear, knn, svm models
-lm_rec <- recipe(price_log10 ~ ., data = airbnb_train) |> 
+# Naive bayes recipe ----
+nb_rec <- recipe(host_is_superhost ~ ., data = airbnb_train) |> 
   step_rm(
     where(lubridate::is.Date), description, host_location, host_about, 
     host_neighbourhood, neighbourhood_cleansed, property_type, bathrooms_text,
     amenities, starts_with("review_scores"), reviews_per_month
   ) |> 
-  step_rm(price, skip = TRUE) |> 
   step_impute_median(all_numeric_predictors()) |> 
-  step_unknown(host_response_time) |> 
+  step_impute_mode(all_nominal_predictors()) |> 
+  step_nzv(all_predictors()) |> 
+  step_normalize(all_numeric_predictors())
+
+# # check recipe
+# nb_rec |>
+#   prep() |>
+#   bake(new_data = NULL) |>
+#   skimr::skim_without_charts()
+
+# save recipe
+save(nb_rec, file = here("attempt_1/recipes/nb_rec.rda"))
+
+# Linear recipe ----
+
+# recipe for linear, knn, svm models
+lm_rec <- recipe(host_is_superhost ~ ., data = airbnb_train) |> 
+  step_rm(
+    where(lubridate::is.Date), description, host_location, host_about, 
+    host_neighbourhood, neighbourhood_cleansed, property_type, bathrooms_text,
+    amenities, starts_with("review_scores"), reviews_per_month
+  ) |> 
+  step_impute_median(all_numeric_predictors()) |> 
   step_impute_mode(all_nominal_predictors()) |> 
   step_dummy(all_nominal_predictors()) |> 
   step_nzv(all_predictors()) |> 
@@ -45,15 +64,13 @@ save(lm_rec, file = here("attempt_1/recipes/lm_rec.rda"))
 # Tree recipe ----
 
 # recipe for knn, random forest, boosted tree, neural network models
-tree_rec <- recipe(price_log10 ~ ., data = airbnb_train) |> 
+tree_rec <- recipe(host_is_superhost ~ ., data = airbnb_train) |> 
   step_rm(
-    where(lubridate::is.Date), description, host_location, host_about, host_neighbourhood,
-    neighbourhood_cleansed, property_type, bathrooms_text, amenities,
-    starts_with("review_scores"), reviews_per_month
+    where(lubridate::is.Date), description, host_location, host_about, 
+    host_neighbourhood, neighbourhood_cleansed, property_type, bathrooms_text,
+    amenities, starts_with("review_scores"), reviews_per_month
   ) |> 
-  step_rm(price, skip = TRUE) |> 
   step_impute_median(all_numeric_predictors()) |> 
-  step_unknown(host_response_time) |> 
   step_impute_mode(all_nominal_predictors()) |> 
   step_dummy(all_nominal_predictors(), one_hot = TRUE) |> 
   step_nzv(all_predictors()) |> 
